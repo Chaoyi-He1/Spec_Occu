9c2089dcb6714c54119bff496ba086cf7bb67fc1
Chaoyi He <chaoyi_he@tamu.edu>
2023-06-16 17:43:52 -0500

    modified:   models/contrastive_model.py

--- models/contrastive_model.py
+++ models/contrastive_model.py
@@ -77,7 +77,7 @@ class Conv2d_BN_Relu(nn.Sequential):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
         super(Conv2d_BN_Relu, self).__init__(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation),
-            nn.BatchNorm1d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
