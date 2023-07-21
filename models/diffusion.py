b0dbe8d58dfef5001bfb7d884e0cba2d70551857
Chaoyi He <chaoyi_he@tamu.edu>
2023-07-21 17:28:48 -0500

    modified:   models/diffusion.py 	modified:   train_eval/train_eval_diffusion.py 	new file:   util/diffusion.py

--- models/diffusion.py
+++ models/diffusion.py
@@ -4,7 +4,6 @@ from torch.nn import Module, Parameter, ModuleList
 import numpy as np
 import torch.nn as nn
 from .common import *
-import pdb
 from .positional_embedding import *
 from .transformer import *
 
@@ -77,88 +76,6 @@ class VarianceSchedule(Module):
         sigmas = self.sigmas_flex[t] * flexibility + self.sigmas_inflex[t] * (1 - flexibility)
         return sigmas
 
-
-class DiffusionTraj(Module):
-    def __init__(self, model, var_sched:VarianceSchedule):
-        super().__init__()
-        self.model = model
-        self.var_sched = var_sched
-
-    def get_loss(self, x_0, context, t=None):
-        """
-        Diffusion loss.
-        Based on Denoising Diffusion Probabilistic Models
-        equation (14) in
-        https://arxiv.org/abs/2006.11239
-        Loss = ||\epsilon - \epsilon_theta(\sqrt(\alpha_bar_t x0) + \sqrt(1 - \alpha_bar_t \epsilon)
-                                          , t)||^2
-        """
-        batch_size, _, point_dim = x_0.size()
-        if t == None:
-            t = self.var_sched.uniform_sample_t(batch_size)
-
-        alpha_bar = self.var_sched.alpha_bars[t]
-        beta = self.var_sched.betas[t].cuda()
-
-        c0 = torch.sqrt(alpha_bar).view(-1, 1, 1).cuda()       # (B, 1, 1)
-        c1 = torch.sqrt(1 - alpha_bar).view(-1, 1, 1).cuda()   # (B, 1, 1)
-
-        e_rand = torch.randn_like(x_0).cuda()  # (B, N, d)
-
-        e_theta = self.model(c0 * x_0 + c1 * e_rand, beta=beta, context=context)
-        loss = F.mse_loss(e_theta, e_rand, reduction='mean')
-        return loss
-
-    def sample(self, num_points, context, sample, bestof, 
-               point_dim=2, flexibility=0.0, ret_traj=False, sampling="ddpm", step=1):
-        """
-        Sample from the diffusion model.
-        DDPM: Denoising Diffusion Probabilistic Models
-        https://arxiv.org/abs/2006.11239
-        DDIM: Denoising Diffusion Implicit Models
-        https://arxiv.org/abs/2010.02502
-        """
-        traj_list = []
-        for _ in range(sample):
-            batch_size = context.size(0)
-            if bestof:
-                x_T = torch.randn([batch_size, num_points, point_dim]).to(context.device)
-            else:
-                x_T = torch.zeros([batch_size, num_points, point_dim]).to(context.device)
-            traj = {self.var_sched.num_steps: x_T}
-            stride = step
-
-            for t in range(self.var_sched.num_steps, 0, -stride):
-                z = torch.randn_like(x_T) if t > 1 else torch.zeros_like(x_T)
-                alpha = self.var_sched.alphas[t]
-                alpha_bar = self.var_sched.alpha_bars[t]
-                alpha_bar_next = self.var_sched.alpha_bars[t-stride]    # next: closer to 1
-                # pdb.set_trace()
-                sigma = self.var_sched.get_sigmas(t, flexibility)
-
-                c0 = 1.0 / torch.sqrt(alpha)
-                c1 = (1 - alpha) / torch.sqrt(1 - alpha_bar)
-
-                x_t = traj[t]
-                beta = self.var_sched.betas[[t] * batch_size]
-                e_theta = self.model(x_t, beta=beta, context=context)
-                if sampling == "ddpm":
-                    x_next = c0 * (x_t - c1 * e_theta) + sigma * z
-                elif sampling == "ddim":
-                    x0_t = (x_t - e_theta * (1 - alpha_bar).sqrt()) / alpha_bar.sqrt()
-                    x_next = alpha_bar_next.sqrt() * x0_t + (1 - alpha_bar_next).sqrt() * e_theta
-                else:
-                    pdb.set_trace()
-                traj[t-stride] = x_next.detach()     # Stop gradient and save trajectory.
-                traj[t] = traj[t].cpu()         # Move previous output to CPU memory.
-                if not ret_traj:
-                   del traj[t]
-
-            if ret_traj:
-                traj_list.append(traj)
-            else:
-                traj_list.append(traj[0])
-        return torch.stack(traj_list)
 
 class TrajNet(Module):
 
--- train_eval/train_eval_diffusion.py
+++ train_eval/train_eval_diffusion.py
@@ -1,23 +1,80 @@
 import torch
 import torch.nn as nn
 from util.misc import *
-from util.Contrastive import *
+from util.diffusion import *
 from typing import Iterable
 
 
-def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
-                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, max_norm: float = .01,
-                    scaler=None, steps: int = 12):
+def train_one_epoch(encoder: torch.nn.Module, model: torch.nn.Module, 
+                    criterion: Diffusion_utils, data_loader: Iterable, 
+                    optimizer: torch.optim.Optimizer, device: torch.device, 
+                    epoch: int, max_norm: float = .01, scaler=None):
+    encoder.train()
     model.train()
     criterion.train()
-    Step_Predict = Step_Prediction(num_steps=steps)
     metric_logger = MetricLogger(delimiter="; ")
     metric_logger.add_meter('loss', SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    
+    metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+
+    for _, (history, future) in enumerate(metric_logger.log_every(data_loader, 10, header)):
+        history = history.to(device)
+        future = future.to(device)
+
+        # Compute the output
+        with torch.cuda.amp.autocast(enabled=scaler is not None):
+            features = encoder(history)
+            BCELoss = criterion.get_loss(x_0=future, context=features, model=model)
+
+        if torch.isnan(BCELoss):
+            raise ValueError('NaN loss detected')
+            
+        # reduce losses over all GPUs for logging purposes
+        BCELoss_reduced = reduce_loss(BCELoss)
+
+        # Backward
+        optimizer.zero_grad()
+        if scaler is not None:
+            scaler.scale(BCELoss).backward()
+        else:
+            BCELoss.backward()
+        
+        if max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+        
+        if scaler is not None:
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            optimizer.step()
+        
+        # Update metric
+        metric_logger.update(loss=BCELoss_reduced.item())
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
-def evaluate(model: torch.nn.Module, criterion: torch.nn.Module,
-             data_loader: Iterable, device: torch.device, epoch: int,
-             steps: int = 12):
-    pass
+def evaluate(encoder: torch.nn.Module, model: torch.nn.Module, 
+             criterion: Diffusion_utils, data_loader: Iterable, 
+             device: torch.device, scaler=None, repeat=20):
+    encoder.eval()
+    model.eval()
+    criterion.eval()
+    metric_logger = MetricLogger(delimiter="; ")
+    metric_logger.add_meter('loss', SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Test:'
+
+    for _, (history, future) in enumerate(metric_logger.log_every(data_loader, 10, header)):
+        history = history.to(device)
+        future = future.to(device)
+        b, l, d = future.shape
+
+        # Compute the output
+        with torch.cuda.amp.autocast(enabled=scaler is not None), torch.no_grad():
+            features = encoder(history)
+            predict = criterion.sample()
