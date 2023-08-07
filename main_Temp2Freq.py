dcbd84e4f060cff2177db43c2a1995047d59a85e
Chaoyi He <chaoyi_he@tamu.edu>
2023-08-06 22:03:22 -0500

    modified:   main_Temp2Freq.py

--- main_Temp2Freq.py
+++ main_Temp2Freq.py
@@ -37,7 +37,7 @@ def get_args_parser():
     parser.add_argument('--eval', action='store_true', help='only evaluate model on validation set')
 
     # Model parameters
-    parser.add_argument('--resume', type=str, default='weights/model_076.pth', help="initial weights path")  # weights/model_940.pth
+    parser.add_argument('--resume', type=str, default='', help="initial weights path")  # weights/model_940.pth
     parser.add_argument('--time-step', type=int, default=12, help="number of time steps to predict")
     parser.add_argument('--hpy', type=str, default='cfg/cfg.yaml', help="hyper parameters path")
     parser.add_argument('--positional-embedding', default='sine', choices=('sine', 'learned'),
@@ -49,7 +49,7 @@ def get_args_parser():
     parser.add_argument('--lr', default=0.001, type=float)
     parser.add_argument('--lrf', default=0.01, type=float)
     parser.add_argument('--weight_decay', default=0.0, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
+    parser.add_argument('--epochs', default=500, type=int)
     parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--num_workers', default=8, type=int)
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N', help='start epoch')
