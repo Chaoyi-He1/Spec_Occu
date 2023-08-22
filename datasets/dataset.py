e8804fa229365e31641755352d04a52dea580e95
Chaoyi-He1 <chaoyi_he@tamu.edu>
2023-08-22 14:50:15 -0500

    update

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 
 class Contrastive_data(Dataset):
     def __init__(self, data_path: str = "", cache: bool = True, past_steps: int = 32,
-                 future_steps: int = 12, train_split: float = 0.8, train: bool = True, 
+                 future_steps: int = 12, train_split: float = 0.8, train: bool = True,
                  num_frames_per_clip: int = 256, temp_dim: int = 1024) -> None:
         super(Contrastive_data, self).__init__()
         assert os.path.isfile(data_path), "path '{}' does not exist.".format(data_path)
@@ -27,7 +27,7 @@ class Contrastive_data(Dataset):
         self.num_frames_per_clip = num_frames_per_clip
 
         self.data, self.data_len, self.train_len = self.cache_data(cache)
-    
+
     def h5py_to_dict(self, h5_obj):
         if isinstance(h5_obj, h5py.File) or isinstance(h5_obj, h5py.Group):
             data = {}
@@ -50,13 +50,13 @@ class Contrastive_data(Dataset):
         #     data[k] = v[:100000, :]
         if cache:
             data = np.stack([data["data_frame_I"], data["data_frame_Q"]], axis=1)
-            train_len = int(data.shape[0] * self.train_split) 
+            train_len = int(data.shape[0] * self.train_split)
             data = data[:train_len, :, :] if self.train else data[train_len:, :, :]
             data = data[:-(data.shape[0] % self.num_frames_per_clip), :, :]
 
             data = np.nan_to_num(data, nan=0.)
             data = np.reshape(data, (-1, self.num_frames_per_clip, 2, self.temp_dim))
-            data = data.transpose(0, 2, 1, 3).copy() 
+            data = data.transpose(0, 2, 1, 3).copy()
             return data, \
                    data.shape[0], int(train_len // self.num_frames_per_clip)
         else:
@@ -64,10 +64,9 @@ class Contrastive_data(Dataset):
             data_len = train_len if self.train else data["data_frame_I"].shape[0] - train_len
             return None, int(data_len // self.num_frames_per_clip), int(train_len // self.num_frames_per_clip)
 
-        
     def __len__(self):
         return self.data_len - self.time_len + 1
-    
+
     def __getitem__(self, index):
         """
         Args:
@@ -80,25 +79,25 @@ class Contrastive_data(Dataset):
             index = (self.train_len + index) * self.num_frames_per_clip \
                 if not self.train else index * self.num_frames_per_clip
             with h5py.File(self.data_path, "r") as f:
-                real = f["data_frame_I"][index:index + 
-                                         self.time_len * self.num_frames_per_clip, :]
+                real = f["data_frame_I"][index:index +
+                                               self.time_len * self.num_frames_per_clip, :]
                 real = np.reshape(real, (self.time_len, self.num_frames_per_clip, self.temp_dim))
-                imag = f["data_frame_Q"][index:index + 
-                                         self.time_len * self.num_frames_per_clip, :]
+                imag = f["data_frame_Q"][index:index +
+                                               self.time_len * self.num_frames_per_clip, :]
                 imag = np.reshape(imag, (self.time_len, self.num_frames_per_clip, self.temp_dim))
-                data_past = np.stack([real[index:index+self.past_steps, :, :], 
-                                      imag[index:index+self.past_steps, :, :]], axis=1)
-                data_future = np.stack([real[index+self.past_steps:index+self.time_len, :, :],
-                                        imag[index+self.past_steps:index+self.time_len, :, :]], axis=1)
-            
+                data_past = np.stack([real[index:index + self.past_steps, :, :],
+                                      imag[index:index + self.past_steps, :, :]], axis=1)
+                data_future = np.stack([real[index + self.past_steps:index + self.time_len, :, :],
+                                        imag[index + self.past_steps:index + self.time_len, :, :]], axis=1)
+
             return np.nan_to_num(data_past, nan=0.), np.nan_to_num(data_future, nan=0.)
-        
+
         else:
-            data_past = self.data[index:index+self.past_steps, :, :, :]
-            data_future = self.data[index+self.past_steps:index+self.time_len, :, :, :]
-        
+            data_past = self.data[index:index + self.past_steps, :, :, :]
+            data_future = self.data[index + self.past_steps:index + self.time_len, :, :, :]
+
         return data_past, data_future
-    
+
     @staticmethod
     def collate_fn(batch):
         data_past, data_future = list(zip(*batch))
@@ -118,7 +117,7 @@ class Contrastive_data(Dataset):
         data_future = torch.cat([data_future_f, data_future_f], dim=2)
 
         return data_past, data_future
-    
+
 
 class Contrastive_data_multi_env(Dataset):
     """
@@ -127,8 +126,9 @@ class Contrastive_data_multi_env(Dataset):
     Each file contains the rx signal for I and Q channel.
     One sampled timeframe is stored in one row
     """
+
     def __init__(self, data_folder_path: str = "", cache: bool = True, in_type: str = "1d",
-                 past_steps: int = 32, future_steps: int = 12, train: bool = True, 
+                 past_steps: int = 32, future_steps: int = 12, train: bool = True,
                  num_frames_per_clip: int = 256, temp_dim: int = 1024) -> None:
         super(Contrastive_data_multi_env, self).__init__()
 
@@ -146,7 +146,7 @@ class Contrastive_data_multi_env(Dataset):
         self.temp_dim = temp_dim
 
         self.data_dict, self.data_len, self.min_len = self.cache_data()
-    
+
     def h5py_to_dict(self, h5_obj):
         if isinstance(h5_obj, h5py.File) or isinstance(h5_obj, h5py.Group):
             data = {}
@@ -160,7 +160,7 @@ class Contrastive_data_multi_env(Dataset):
                 return h5_obj[()]
         else:
             return h5_obj
-        
+
     def preprocess_data(self, data):
         """
         Preprocess the data to reshape it as a 3D input for the model.
@@ -190,7 +190,7 @@ class Contrastive_data_multi_env(Dataset):
             #                     data_f_imag[:, np.newaxis, :]], axis=1)
             # data = data[:-(data.shape[0] % self.num_frames_per_clip), :, :]
             if self.in_type == "2d":
-                data = data.reshape(-1, self.num_frames_per_clip, 2, self.temp_dim).\
+                data = data.reshape(-1, self.num_frames_per_clip, 2, self.temp_dim). \
                     transpose(0, 2, 1, 3)
             # data = data.astype(np.float32)
         elif isinstance(data, np.ndarray):
@@ -198,31 +198,31 @@ class Contrastive_data_multi_env(Dataset):
             # the data is a 4D matrix with shape (batch, num_frames, 2, temporal_dim)
             # the data is read by __getitem__ function
             # and the preprocessing is called by collate_fn function
-            
+
             real = data[:, :, 0, :]
             imag = data[:, :, 1, :]
             data_c = real + 1j * imag
             data_fft = np.fft.fft(data_c, axis=-1)
             data_f_real = np.real(data_fft)
             data_f_imag = np.imag(data_fft)
-            data = np.concatenate([data, 
-                                   data_f_real[:, :, np.newaxis, :], 
+            data = np.concatenate([data,
+                                   data_f_real[:, :, np.newaxis, :],
                                    data_f_imag[:, :, np.newaxis, :]], axis=2)
             # data = data[:, :-(data.shape[0] % self.num_frames_per_clip), :, :]
             if self.in_type == "2d":
                 b, _, _, _ = data.shape
-                data = data.reshape(b, -1, self.num_frames_per_clip, 4, self.temp_dim).\
+                data = data.reshape(b, -1, self.num_frames_per_clip, 4, self.temp_dim). \
                     transpose(0, 1, 3, 2, 4)
             data = data.astype(np.float32)
 
         return data.copy()
-    
+
     def cache_data(self):
         data_dict = {}
         data_len = {}
         min_len = np.inf
         for i, data_file_path in tqdm(enumerate(self.data_files)):
-            print("Loading data from %s ... (%d / %d)" % 
+            print("Loading data from %s ... (%d / %d)" %
                   (data_file_path, i, len(self.data_files)))
             # if i not in [0, 1, 2, 3]:
             #     continue
@@ -236,7 +236,7 @@ class Contrastive_data_multi_env(Dataset):
                 data_dict[i] = None
                 data_len[i] = data["data_frame_I"].shape[0] // self.num_frames_per_clip \
                               - self.past_steps - self.future_steps + 1 if self.in_type == "2d" \
-                              else data["data_frame_I"].shape[0] - self.past_steps - self.future_steps + 1
+                    else data["data_frame_I"].shape[0] - self.past_steps - self.future_steps + 1
             min_len = min(min_len, data_len[i])
         return data_dict, data_len, min_len
 
@@ -258,23 +258,23 @@ class Contrastive_data_multi_env(Dataset):
         time_step = np.random.randint(self.data_len[index])
         if self.cache:
             data = self.data_dict[index]
-            data_past = data[time_step:time_step+self.past_steps, ...]
-            data_future = data[time_step+self.past_steps:
-                               time_step+self.past_steps+self.future_steps, ...]
+            data_past = data[time_step:time_step + self.past_steps, ...]
+            data_future = data[time_step + self.past_steps:
+                               time_step + self.past_steps + self.future_steps, ...]
         else:
             with h5py.File(self.data_files[index], 'r') as f:
                 data = self.h5py_to_dict(f)
             data = np.stack([data["data_frame_I"], data["data_frame_Q"]], axis=1)
             if self.in_type == "2d":
-                data = data[time_step*self.num_frames_per_clip:
-                            (time_step+self.past_steps+self.future_steps)*self.num_frames_per_clip, :, :]
+                data = data[time_step * self.num_frames_per_clip:
+                            (time_step + self.past_steps + self.future_steps) * self.num_frames_per_clip, :, :]
             else:
-                data = data[time_step:time_step+self.past_steps+self.future_steps, :, :]
+                data = data[time_step:time_step + self.past_steps + self.future_steps, :, :]
             data = self.preprocess_data(data)
-            data_past = data[:self.past_steps*self.num_frames_per_clip, ...]
-            data_future = data[self.past_steps*self.num_frames_per_clip:, ...]
+            data_past = data[:self.past_steps * self.num_frames_per_clip, ...]
+            data_future = data[self.past_steps * self.num_frames_per_clip:, ...]
         return data_past, data_future, index
-    
+
     @staticmethod
     def collate_fn(batch):
         """
@@ -295,20 +295,20 @@ class Contrastive_data_multi_env(Dataset):
 
 
 class Temporal_to_Freq_data(Dataset):
-    def __init__(self, data_path: str = "", cache_data: bool = True, 
+    def __init__(self, data_path: str = "", cache_data: bool = True,
                  time_step: int = 12) -> None:
         super(Temporal_to_Freq_data, self).__init__()
         assert os.path.isfile(data_path), "path '{}' does not exist.".format(data_path)
         self.data_path = data_path
         self.data, self.label = self.cache_data() if cache_data else (None, None)
         self.time_step = time_step
-    
+
     def cache_data(self):
         data = sio.loadmat(self.data_path)
         label = data["label"]
         data = np.stack([data["real"][:, np.newaxis, :], data["imag"][:, np.newaxis, :]], axis=1)
         assert data.shape[0] == label.shape[0], "data and label must have the same length."
-        
+
         return data, label
 
     def __len__(self):
@@ -317,7 +317,7 @@ class Temporal_to_Freq_data(Dataset):
             return data["real"].shape[0] - self.time_step + 1
         else:
             return self.data.shape[0] - self.time_step + 1
-    
+
     def __getitem__(self, index):
         """
         Args:
@@ -328,17 +328,17 @@ class Temporal_to_Freq_data(Dataset):
         """
         if self.data is None:
             with h5py.File(self.data_path, "r") as f:
-                real = f["real"][index:index+self.time_step, :]
-                imag = f["imag"][index:index+self.time_step, :]
+                real = f["real"][index:index + self.time_step, :]
+                imag = f["imag"][index:index + self.time_step, :]
                 data = np.stack([real[:, np.newaxis, :], imag[:, np.newaxis, :]], axis=1)
-                label = f["label"][index:index+self.time_step, :]
+                label = f["label"][index:index + self.time_step, :]
         else:
-            data = self.data[index:index+self.time_step, :, :]
-            label = self.label[index:index+self.time_step, :]
-        
+            data = self.data[index:index + self.time_step, :, :]
+            label = self.label[index:index + self.time_step, :]
+
         assert data.shape[0] == label.shape[0], "data and label must have the same length."
         return data, label
-    
+
     @staticmethod
     def collate_fn(batch):
         data, label = list(zip(*batch))
@@ -346,7 +346,7 @@ class Temporal_to_Freq_data(Dataset):
 
 
 class Temporal_to_Freq_data_multi_env(Dataset):
-    def __init__(self, data_folder_path: str = "", cache: bool = True, 
+    def __init__(self, data_folder_path: str = "", cache: bool = True,
                  time_step: int = 12) -> None:
         super(Temporal_to_Freq_data_multi_env, self).__init__()
         assert os.path.isdir(data_folder_path), "path '{}' does not exist.".format(data_folder_path)
@@ -357,7 +357,7 @@ class Temporal_to_Freq_data_multi_env(Dataset):
         self.time_step = time_step
         (self.data_dict, self.label_dict,
          self.data_len, self.min_len) = self.cache_data()
-    
+
     def h5py_to_dict(self, h5_obj):
         if isinstance(h5_obj, h5py.File) or isinstance(h5_obj, h5py.Group):
             data = {}
@@ -371,26 +371,26 @@ class Temporal_to_Freq_data_multi_env(Dataset):
                 return h5_obj[()]
         else:
             return h5_obj
-    
+
     def cache_data(self):
         data_dict = {}
         label_dict = {}
         data_len = {}
         min_len = np.inf
         for i, data_file_path in enumerate(self.data_files):
-            print("Loading data from %s ... (%d / %d)" % 
+            print("Loading data from %s ... (%d / %d)" %
                   (data_file_path, i, len(self.data_files)))
             # if ("20" not in data_file_path) and ("21" not in data_file_path):
             #     continue
             # continue
             with h5py.File(data_file_path, 'r') as f:
                 data = self.h5py_to_dict(f)
-            
+
             if self.cache:
-                start_index = np.random.randint(0, 
+                start_index = np.random.randint(0,
                                                 data["data_frame_I"].shape[0] - 20000)
                 label = data["label_frame"][start_index:start_index + 10000, :]
-                data = np.stack([data["data_frame_I"][start_index:start_index + 10000, :], 
+                data = np.stack([data["data_frame_I"][start_index:start_index + 10000, :],
                                  data["data_frame_Q"][start_index:start_index + 10000, :]], axis=1)
                 assert data.shape[0] == label.shape[0], "data and label must have the same length."
                 data_dict[i] = data.astype(np.float16)
@@ -405,7 +405,7 @@ class Temporal_to_Freq_data_multi_env(Dataset):
 
     def __len__(self):
         return len(self.label_dict) * 768
-    
+
     def __getitem__(self, index):
         """
         Args:
@@ -428,7 +428,7 @@ class Temporal_to_Freq_data_multi_env(Dataset):
             data = self.data_dict[index][time_step:time_step + self.time_step, ...]
             label = self.label_dict[index][time_step:time_step + self.time_step, ...]
         return data, label
-    
+
     @staticmethod
     def collate_fn(batch):
         data, label = list(zip(*batch))
@@ -439,8 +439,8 @@ class Temporal_to_Freq_data_multi_env(Dataset):
 
 
 class Diffusion_multi_env(Dataset):
-    def __init__(self, data_folder_path: str = "", cache: bool = True, 
-                 past_steps: int = 32, future_steps: int = 12, train: bool = True, 
+    def __init__(self, data_folder_path: str = "", cache: bool = True,
+                 past_steps: int = 32, future_steps: int = 12, train: bool = True,
                  temp_dim: int = 1024) -> None:
         super(Diffusion_multi_env, self).__init__()
         assert os.path.isdir(data_folder_path), "path '{}' does not exist.".format(data_folder_path)
@@ -469,21 +469,21 @@ class Diffusion_multi_env(Dataset):
                 return h5_obj[()]
         else:
             return h5_obj
-    
+
     def cache_data(self):
         data_dict = {}
         label_dict = {}
         data_len = {}
         min_len = np.inf
         for i, data_file_path in enumerate(self.data_files):
-            print("Loading data from %s ... (%d / %d)" % 
+            print("Loading data from %s ... (%d / %d)" %
                   (data_file_path, i, len(self.data_files)))
             # if i != 0:
             #     continue
 
             with h5py.File(data_file_path, 'r') as f:
                 data = self.h5py_to_dict(f)
-            
+
             if self.cache:
                 start_index = np.random.randint(0,
                                                 data["data_frame_I"].shape[0] - 20000)
@@ -500,11 +500,11 @@ class Diffusion_multi_env(Dataset):
                 label_dict[i] = data["label_frame"]
             min_len = min(min_len, data_len[i])
         return data_dict, label_dict, data_len, min_len
-    
+
     def __len__(self):
         return len(self.label_dict) * 192 if self.train \
-               else len(self.label_dict) * 20
-    
+            else len(self.label_dict) * 20
+
     def __getitem__(self, index):
         """
         Args:
@@ -526,13 +526,13 @@ class Diffusion_multi_env(Dataset):
         else:
             data = self.data_dict[index][time_step:time_step + self.total_time_steps, ...]
             label = self.label_dict[index][time_step:time_step + self.total_time_steps, ...]
-        
+
         past_data = data[:self.past_steps, ...]
         future_data = data[self.past_steps:, ...]
         past_label = label[:self.past_steps, ...]
         future_label = label[self.past_steps:, ...]
         return past_data, past_label, future_data, future_label
-    
+
     @staticmethod
     def collate_fn(batch):
         past_data, past_label, future_data, future_label = list(zip(*batch))
