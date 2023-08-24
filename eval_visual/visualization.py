40c82e40a88c5097cd0e10110b614a9d210161d9
Chaoyi He <chaoyi_he@tamu.edu>
2023-08-24 13:37:17 -0500

    modified:   eval_visual/visualization.py 	modified:   main_contrast.py

--- eval_visual/visualization.py
+++ eval_visual/visualization.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
 import numpy as np
 import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.stats import multivariate_normal
 
 
 def plot_predictions(ax,
@@ -16,4 +20,24 @@ def plot_predictions(ax,
                      kde=False):
     r, b, l, d = prediction_temporal.shape
     if kde:
-        pass
+        # Create a mesh grid for the (l, c) coordinates
+        data = prediction_temporal.sum(axis=0).squeeze().detach().cpu().numpy()
+
+        # Flatten the (l, c) coordinates and data for KDE
+        values = data.ravel()
+
+        # Create a KDE using SciPy's multivariate_normal
+        kde = multivariate_normal.pdf(values, mean=np.mean(values), cov=np.cov(values))
+
+        # Reshape the KDE values back to (l, c)
+        kde = kde.reshape((l, c))
+
+        # Plot the surface
+        l, c = np.meshgrid(np.arange(l), np.arange(c))
+        ax.plot_surface(l, c, kde.T, cmap='viridis')
+
+        # Customize the plot as needed
+        ax.set_xlabel('L')
+        ax.set_ylabel('C')
+        ax.set_zlabel('KDE Value')
+        ax.set_title('2D KDE in 3D Plot')
