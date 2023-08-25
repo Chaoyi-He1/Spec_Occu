1288c54d568f10fe5de6db6fb15879b482125d96
Chaoyi He <chaoyi_he@tamu.edu>
2023-08-25 13:29:13 -0500

    modified:   eval_visual/visualization.py 	modified:   util/Temp_to_Freq.py

--- util/Temp_to_Freq.py
+++ util/Temp_to_Freq.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch import Tensor
 import torch.nn.functional as F
 import numpy as np
+from typing import List
 
 
 class Steps_BCELoss(nn.Module):
@@ -74,3 +75,27 @@ def Steps_Accuracy(pred: Tensor, targets: Tensor):
         acc_steps.append(torch.eq(F.sigmoid(pred[:, i, :]).round(), 
                                   targets[:, i, :]).sum().float() / (b * n))
     return torch.stack(acc_steps)
+
+
+@torch.no_grad()
+def F1_score(pred: Tensor, targets: Tensor) -> List:
+    """
+    Calculate the F1 score of the temporal to frequency labels.
+    :param pred: Tensor [B, time_step, num_freq]
+    :param targets: Tensor [B, time_step, num_freq], 
+        targets is the ground truth of the frequency occupancy labels
+
+    :return: F1_score: Tensor, the temporal to frequency F1 score
+    """
+    b, l, n = targets.shape
+
+    assert pred.shape == (b, l, n), \
+        "pred shape should be [B, time_step, num_freq]"
+
+    F1_scores = []
+    for i in range(l):
+        tp = (F.sigmoid(pred[:, i, :]).round() * targets[:, i, :]).sum().float()
+        fp = (F.sigmoid(pred[:, i, :]).round() * (1 - targets[:, i, :])).sum().float()
+        fn = ((1 - F.sigmoid(pred[:, i, :]).round()) * targets[:, i, :]).sum().float()
+        F1_scores.append(tp / (tp + 0.5 * (fp + fn)))
+    return F1_scores
