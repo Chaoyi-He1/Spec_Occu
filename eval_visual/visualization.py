4f84eef06aa752d94f856a7550eb5ea74202d5d9
Chaoyi He <chaoyi_he@tamu.edu>
2023-08-25 13:29:31 -0500

    modified:   eval_visual/visualization.py 	modified:   util/Temp_to_Freq.py

--- eval_visual/visualization.py
+++ eval_visual/visualization.py
@@ -8,15 +8,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
 
 
-def plot_predictions(ax,
+def plot_predictions(ax, fig,
                      prediction_temporal,
                      futures_label,
-                     line_alpha=0.7,
-                     line_width=0.2,
-                     edge_width=2,
-                     circle_edge_width=0.5,
-                     node_circle_size=0.3,
-                     batch_num=0,
                      kde=False):
     r, b, l, d = prediction_temporal.shape
     if kde:
@@ -30,14 +24,40 @@ def plot_predictions(ax,
         kde = multivariate_normal.pdf(values, mean=np.mean(values), cov=np.cov(values))
 
         # Reshape the KDE values back to (l, c)
-        kde = kde.reshape((l, c))
+        kde = kde.reshape((l, d))
 
         # Plot the surface
-        l, c = np.meshgrid(np.arange(l), np.arange(c))
-        ax.plot_surface(l, c, kde.T, cmap='viridis')
+        l, d = np.meshgrid(np.arange(l), np.arange(d))
+        ax.plot_surface(l, d, kde.T, cmap='viridis')
 
         # Customize the plot as needed
         ax.set_xlabel('L')
-        ax.set_ylabel('C')
+        ax.set_ylabel('F')
         ax.set_zlabel('KDE Value')
         ax.set_title('2D KDE in 3D Plot')
+        ax.set_aspect('equal', adjustable='box')
+        
+    
+    # Create a scatter plot with the 'Blues' colormap
+    data = prediction_temporal.sum(axis=0).squeeze().detach().cpu().numpy()
+    flat_data = data.flatten()
+    scatter = ax.scatter(
+        np.tile(np.arange(d), l),  # X-axis values
+        np.repeat(np.arange(l), d),  # Y-axis values
+        c=flat_data,  # Color based on data values
+        cmap='Blues',  # Colormap ('Blues' for dark to bright blue)
+        marker='s',  # Marker style (square)
+        s=50,  # Marker size
+    )
+
+    # Set labels and title
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_title('2D Probability Cloud')
+
+    # Add a color bar
+    cbar = fig.colorbar(scatter, ax=ax, label='Probability')
+
+    # Set the aspect ratio to equal for square cells
+    ax.set_aspect('equal', adjustable='box')
+
