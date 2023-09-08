ed15cb6e91eb5b16534e42f20741783dc2b43436
Chaoyi He <chaoyi_he@tamu.edu>
2023-09-08 14:47:30 -0500

    modified:   main_whole_fine_tune.py 	modified:   train_eval/evaluation.py

--- train_eval/evaluation.py
+++ train_eval/evaluation.py
@@ -22,6 +22,7 @@ def evaluate(encoder: torch.nn.Module, diff_model: torch.nn.Module,
     metric_logger = MetricLogger(delimiter="; ")
     metric_logger.add_meter('loss', SmoothedValue(window_size=1, fmt='{value:.6f}'))
     metric_logger.add_meter('acc', SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    metric_logger.add_meter('acc_steps', SmoothedValue(window_size=1, fmt='{value:.6f}'))
     metric_logger.add_meter('F1_score', SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Test:'
     
@@ -44,19 +45,37 @@ def evaluate(encoder: torch.nn.Module, diff_model: torch.nn.Module,
                                             ret_traj=False, sampling="ddpm") / 50
             
             predict_labels = [T2F_model(predict) for predict in predicts]
-            BCELoss, acc_steps = zip(*[T2F_criterion(predict_label, future_labels) for predict_label in predict_labels])
-            F1scores = [torch.stack(F1_score(predict_label, future_labels))
-                        for predict_label in predict_labels]
-            acc = torch.stack(acc_steps).mean()
-            F1score = torch.stack(F1scores).mean(dim=0)
+            acc_steps, acc, F1score, predict_probs = calculate_prob_cloud(predict_labels, future_labels)
+            BCELoss, _ = zip(*[T2F_criterion(predict_label, future_labels) for predict_label in predict_labels])
+            
         
         if F1score.mean() > best_F1score and acc > best_acc:
             best_history, best_hist_labels, best_future, best_future_labels = history, hist_labels, future, future_labels
             best_acc, best_F1score = acc, F1score.mean()
             
         metric_logger.update(loss=torch.stack(BCELoss).mean().item(), 
-                             acc=acc.item(), F1_score=F1score.mean().item())
+                             acc=acc.item(), acc_steps=acc_steps.item(),
+                             F1_score=F1score.mean().item())
     
     return best_history, best_hist_labels, best_future, best_future_labels, \
            {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+def calculate_prob_cloud(predict_labels: List[Tensor], future_labels: Tensor, 
+                         threshold: float = 0.5):
+    predict_labels = torch.sigmoid(torch.stack(predict_labels)).round()
+    sample_size, num_time_steps, num_classes = predict_labels.shape
+    predict_probs = predict_labels.sum(dim=0) / sample_size
+    predict_labels = predict_probs.clone()
+    predict_labels[predict_probs < threshold] = 0
+    predict_labels[predict_probs >= threshold] = 1
+    
+    # calculate the accuracy and F1 score
+    acc_steps = torch.eq(predict_labels, future_labels).sum(dim=1).float() / num_classes
+    acc = acc_steps.mean()
+    tp = torch.logical_and(predict_labels == 1, future_labels == 1).sum(dim=1).float()
+    fp = torch.logical_and(predict_labels == 1, future_labels == 0).sum(dim=1).float()
+    fn = torch.logical_and(predict_labels == 0, future_labels == 1).sum(dim=1).float()
+    F1scores = tp / (tp + 0.5 * (fp + fn))
+    return acc_steps, acc, F1scores, predict_probs
     
