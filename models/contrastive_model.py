42cb1ff81be60a82ba5b2665c546118fe9620203
Chaoyi He <chaoyi_he@tamu.edu>
2024-02-06 15:35:28 -0600

    modified:   cfg/cfg.yaml 	modified:   datasets/dataset.py 	new file:   main_feature_cls.py 	modified:   models/contrastive_model.py 	new file:   train_eval/train_eval_feature_cls.py

--- models/contrastive_model.py
+++ models/contrastive_model.py
@@ -626,9 +626,26 @@ class Feature_Extractor(nn.Module):
         return feature + self.input_residue_proj(inputs)
 
 
+class Feature_Extractor_cls(nn.Module):
+    def __init__(self, cfg: dict = None, pos_type: str = "sine", 
+                 in_type: str = "1d") -> None:
+        super().__init__()
+        
+        self.feature_extractor = Feature_Extractor(cfg, pos_type, in_type)
+        self.cls_head = nn.Linear(cfg["feature_dim"], cfg["num_classes"])
+    
+    def forward(self, inputs: Tensor) -> Tensor:
+        feature = self.feature_extractor(inputs)
+        return self.cls_head(feature)
+
+
 def build_contrastive_model(cfg: dict = None, timestep: int = 12, pos_type: str = "sine") -> nn.Module:
     return Encoder_Regressor(cfg, timestep, pos_type)
 
 
 def build_feature_extractor(cfg: dict = None, pos_type: str = "sine", in_type: str = "1d") -> nn.Module:
     return Feature_Extractor(cfg, pos_type, in_type)
+
+
+def build_feature_extractor_cls(cfg: dict = None, pos_type: str = "sine", in_type: str = "1d") -> nn.Module:
+    return Feature_Extractor_cls(cfg, pos_type, in_type)
