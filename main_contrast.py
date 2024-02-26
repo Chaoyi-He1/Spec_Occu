e83f15700367c3965da63322578175035333902c
Chaoyi He <chaoyi_he@tamu.edu>
2024-02-25 18:23:32 -0600

    modified:   cfg/cfg.yaml 	modified:   main_contrast.py

--- main_contrast.py
+++ main_contrast.py
@@ -48,10 +48,10 @@ def get_args_parser():
     parser.add_argument('--save-best', action='store_true', help="save best model")
 
     # Optimization parameters
-    parser.add_argument('--lr', default=5e-5, type=float)
+    parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--lrf', default=0.01, type=float)
     parser.add_argument('--weight_decay', default=0.0, type=float)
-    parser.add_argument('--epochs', default=1500, type=int)
+    parser.add_argument('--epochs', default=300, type=int)
     parser.add_argument('--batch_size', default=5, type=int)
     parser.add_argument('--num_workers', default=8, type=int)
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N', help='start epoch')
