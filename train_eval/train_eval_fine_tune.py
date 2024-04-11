3f8244b93cd1ba96d010b0c9d619681c12ba738a
Chaoyi He <chaoyi_he@tamu.edu>
2024-04-10 20:39:52 -0500

    modified:   train_eval/train_eval_fine_tune.py

--- train_eval/train_eval_fine_tune.py
+++ train_eval/train_eval_fine_tune.py
@@ -209,7 +209,7 @@ def evaluate(encoder: torch.nn.Module, diff_model: torch.nn.Module,
     ax = fig.add_subplot(111)
     ax.plot(FPRs, TPRs, label="ROC curve for the model")
     for i, txt in enumerate(np.linspace(10, 80, 100)):
-        ax.annotate("{:.2f}".format(txt), (TPRs[i], FPRs[i]))
+        ax.annotate("{:.2f}".format(txt), (FPRs[i] + 0.5, TPRs[i] - 0.5))
     ax.set_xlabel("FP Rate")
     ax.set_ylabel("TP Rate")
     ax.set_title("ROC curve")
